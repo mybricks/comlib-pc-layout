0f68c3e1c6ebe39c8c9740f80fbcaecf2b29b94b
wudi27 <wudi27@kuaishou.com>
2024-03-25 20:03:14 +0800

    feat: 确保子应用渲染器在发布后不报错

--- src/micro-app-renderer/utils/qiankun-polyfill.ts
+++ src/micro-app-renderer/utils/qiankun-polyfill.ts
@@ -1,67 +1,9 @@
 import * as qiankun from 'qiankun';
 import { SimpleMicroApp, LoadableApp } from '../types';
 
-// export interface MApp {
-//   name: string;
-//   entry: string;
-//   container: HTMLElement;
-//   activeRule?: string;
-// }
-
-// interface AppItem {
-//   [url: string]: MApp;
-// }
-
 class MainApp {
-  // apps: MApp[] = [] as MApp[];
-
-  // appsMap: AppItem = {};
 
   cacheHtmlMap = {};
-
-  // isHashMode = true;
-
-  // curUrl = '';
-
-  // regist = ({ apps }: { apps: MApp[] }) => {
-  //   this.apps = apps;
-
-  //   apps.forEach((app) => {
-  //     this.appsMap[app.activeRule] = app;
-  //   });
-  // };
-
-  // start = () => {};
-
-  // goto = (url, title) => {
-  //   this.curUrl = url;
-  //   const curApp = this.appsMap[this.curUrl];
-
-  //   if (!curApp || !curApp.container || !curApp.entry) {
-  //     console.warn('[micro app load fail] invalid app');
-  //     return;
-  //   }
-
-  //   const { entry, container } = curApp;
-
-  //   if (!this.cacheHtmlMap?.[entry]) {
-  //     fetch(entry, {
-  //       method: 'GET'
-  //     })
-  //       .then((res) => {
-  //         if (res.headers.get('content-type') === 'text/plain') {
-  //           return res.text();
-  //         }
-  //         return res.text();
-  //       })
-  //       .then((htmlString: string) => {
-  //         this.cacheHtmlMap[entry] = htmlString;
-  //         this.renderHtmlByHtmlString(htmlString, container);
-  //       });
-  //   } else {
-  //     this.renderHtmlByHtmlString(this.cacheHtmlMap[entry], container);
-  //   }
-  // };
 
   loadApp = ({ entry, container }: { entry: string; container: HTMLElement }): SimpleMicroApp => {
     if (!this.cacheHtmlMap?.[entry]) {
@@ -83,7 +25,7 @@ class MainApp {
               .replaceAll('src="public', `src="${prefix}/public`)
               .replaceAll('href="public', `href="${prefix}/public`);
             this.cacheHtmlMap[entry] = finalHtmlString;
-            this.renderHtmlByHtmlString(finalHtmlString, container);
+            this.renderHtmlByHtmlString(encodeURIComponent(finalHtmlString), container);
           });
         return null;
       };
@@ -100,7 +42,7 @@ class MainApp {
       };
     } else {
       const mount = () => {
-        this.renderHtmlByHtmlString(this.cacheHtmlMap[entry], container);
+        this.renderHtmlByHtmlString(encodeURIComponent(this.cacheHtmlMap[entry]), container);
         return Promise.resolve(null);
       };
 
@@ -117,21 +59,25 @@ class MainApp {
 
   loadInvalidApp = ({ container }: { container: HTMLElement }): SimpleMicroApp => {
     const mount: () => Promise<null> = () => {
+      // this.renderHtmlByHtmlString(
+      //   `
+      //   <body style="margin: 0px;">
+      //     <div style="
+      //       display: flex;
+      //       justify-content: center;
+      //       align-items: center;
+      //       font-style: italic;
+      //       color: #999;
+      //       font-weight: 400;
+      //       font-size: 13px;
+      //       height: 50px;
+      //     ">当前页面未配置</div>
+      //   </body>
+      //   `,
+      //   container
+      // );
       this.renderHtmlByHtmlString(
-        `
-        <body style="margin: 0px;">
-          <div style="
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            font-style: italic;
-            color: #999;
-            font-weight: 400;
-            font-size: 13px;
-            height: 50px;
-          ">当前页面未配置</div>
-        </body>
-        `,
+        `%0A%20%20%20%20%20%20%20%20%3Cbody%20style%3D%22margin%3A%200px%3B%22%3E%0A%20%20%20%20%20%20%20%20%20%20%3Cdiv%20style%3D%22%0A%20%20%20%20%20%20%20%20%20%20%20%20display%3A%20flex%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20justify-content%3A%20center%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20align-items%3A%20center%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-style%3A%20italic%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20color%3A%20%23999%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-weight%3A%20400%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20font-size%3A%2013px%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20height%3A%2050px%3B%0A%20%20%20%20%20%20%20%20%20%20%22%3E%E5%BD%93%E5%89%8D%E9%A1%B5%E9%9D%A2%E6%9C%AA%E9%85%8D%E7%BD%AE%3C%2Fdiv%3E%0A%20%20%20%20%20%20%20%20%3C%2Fbody%3E%0A%20%20%20%20%20%20%20%20`,
         container
       );
       return Promise.resolve(null);
@@ -152,23 +98,27 @@ class MainApp {
       mountNode.innerHTML = '';
       mountNode.appendChild(iframeEle);
 
+      // htmlString = htmlString.replace(
+      //   '</head>',
+      //   `
+      //   <script>
+      //     var parentHistory = window.parent.history;
+      //     history.pushState = (...args) => {
+      //       return parentHistory.pushState.apply(parentHistory, args)
+      //     }
+      //   </script>
+      //   </head>
+      // `
+      // );
       htmlString = htmlString.replace(
-        '</head>',
-        `
-        <script>
-          var parentHistory = window.parent.history;
-          history.pushState = (...args) => {
-            return parentHistory.pushState.apply(parentHistory, args)
-          }
-        </script>
-        </head>
-      `
+        `%3C%2Fhead%3E`,
+        `%0A%20%20%20%20%20%20%20%20%3Cscript%3E%0A%20%20%20%20%20%20%20%20%20%20var%20parentHistory%20%3D%20window.parent.history%3B%0A%20%20%20%20%20%20%20%20%20%20history.pushState%20%3D%20(...args)%20%3D%3E%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20return%20parentHistory.pushState.apply(parentHistory%2C%20args)%0A%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%3C%2Fscript%3E%0A%20%20%20%20%20%20%20%20%3C%2Fhead%3E%0A%20%20%20%20%20%20`
       );
       window.iframeEle = iframeEle;
 
       iframeEle.style = 'width: 100%;height: 100%;border: 0px;border-radius: 0px;';
 
-      iframeEle.contentWindow?.document.write(htmlString);
+      iframeEle.contentWindow?.document.write(decodeURIComponent(htmlString));
     }
   };
 }
@@ -193,43 +143,4 @@ export const loadApp = ({ name, entry, container }: LoadableApp) => {
 
 export const loadInvalidApp = ({ container }: { container: HTMLElement }) => {
   return mockMainApp.loadInvalidApp({ container });
-  // if (openIframeMode) {
-  //   return mockMainApp.loadInvalidApp({ container })
-  // }
-  // return qiankun.loadMicroApp({ name: '__EMPTY__PAGE__', container, entry: {  html: EMPTY_HTML_STRING, styles: [] }  })
 };
-
-// export class RouteManager {
-//   private _envType = location.pathname.startsWith('/runtime/mfs/staging') ? 'staging' : 'prod';
-//   private _rootPath = location.pathname
-//     .split('/')
-//     .slice(0, this._envType === 'staging' ? 8 : 7)
-//     .join('/');
-
-//   onRouteChange: (url: string) => void = (url) => {};
-
-//   init = ({ isHashMode = true, onRouteChange = (url: string) => {} } = {}) => {};
-
-//   getRootPath() {
-//     return this._rootPath;
-//   }
-
-//   getCurrentRoute = () => {
-//     return location.pathname
-//       .split('/')
-//       .slice(this._envType === 'staging' ? 8 : 7)
-//       .join('/');
-//   };
-
-//   goto = (url) => {
-//     this.pushState(url);
-//   };
-
-//   replaceState(url = '') {
-//     history.replaceState(null, '', url ? `${this._rootPath}/${url}` : '');
-//   }
-
-//   pushState(url = '') {
-//     history.pushState(null, '', url);
-//   }
-// }
